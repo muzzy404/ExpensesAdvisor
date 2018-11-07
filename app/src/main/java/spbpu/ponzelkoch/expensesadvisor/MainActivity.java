94240f0a1f7116119649110734c8931f5210fea4
Daria Zelenova <muzzy404@gmail.com>
2018-11-07 22:33:39 +0300

    Initial commit

---
+++ app/src/main/java/spbpu/ponzelkoch/expensesadvisor/MainActivity.java
@@ -0,0 +1,44 @@
+package spbpu.ponzelkoch.expensesadvisor;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_checks_list:
+                    mTextMessage.setText(R.string.title_checks_list);
+                    return true;
+                case R.id.navigation_qr_scanner:
+                    mTextMessage.setText(R.string.title_qr_scanner);
+                    return true;
+                case R.id.navigation_statistics:
+                    mTextMessage.setText(R.string.title_statistics);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
