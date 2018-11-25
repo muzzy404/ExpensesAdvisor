c7692b010d2eb1dbeca2e7e28725969a49f4da23
Daria Zelenova <muzzy404@gmail.com>
2018-11-26 00:35:25 +0300

    qr scanner fragment ready

--- app/src/main/java/spbpu/ponzelkoch/expensesadvisor/MainActivity.java
+++ app/src/main/java/spbpu/ponzelkoch/expensesadvisor/MainActivity.java
@@ -1,9 +1,14 @@
 package spbpu.ponzelkoch.expensesadvisor;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +20,38 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private String username;
+    private final int CAMERA_PERMISSION = 10;
+    public static boolean cameraPermission = false;
+
+    public BottomNavigationView navigation;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        username = intent.getStringExtra(LoginActivity.USERNAME);
+
+        navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_checks_list);
+
+        // camera permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+            }
+        } else {
+            cameraPermission = true;
+        }
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmant_container,
+                                                               fragment).commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_checks_list:
                     setFragment(new ChecksFragment());
+                    result = true;
                     break;
                 case R.id.navigation_qr_scanner:
                     setFragment(new ScanQRFragment());
@@ -40,25 +78,5 @@ public class MainActivity extends AppCompatActivity {
             return result;
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = getIntent();
-        username = intent.getStringExtra(LoginActivity.USERNAME);
-
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
-        setFragment(new ChecksFragment());
-
-        Toast.makeText(this, username, Toast.LENGTH_SHORT).show();
-    }
-
-    private void setFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmant_container,
-                                                               fragment).commit();
-    }
 
 }
--- app/src/main/java/spbpu/ponzelkoch/expensesadvisor/ScanQRFragment.java
+++ app/src/main/java/spbpu/ponzelkoch/expensesadvisor/ScanQRFragment.java
@@ -1,10 +1,20 @@
 package spbpu.ponzelkoch.expensesadvisor;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
@@ -13,6 +23,8 @@ import androidx.fragment.app.Fragment;
  */
 public class ScanQRFragment extends Fragment {
 
+    private CodeScanner checkScanner;
+
 
     public ScanQRFragment() {
         // Required empty public constructor
@@ -20,10 +32,45 @@ public class ScanQRFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan_qr, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_scan_qr, container, false);
+        final Activity activity = getActivity();
+        View root = inflater.inflate(R.layout.fragment_scan_qr, container, false);
+
+        CodeScannerView scannerView = root.findViewById(R.id.scanner_view);
+        checkScanner = new CodeScanner(activity, scannerView);
+        checkScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
+
+                        MainActivity mainActivity = (MainActivity) getActivity();
+                        mainActivity.navigation.setSelectedItemId(R.id.navigation_checks_list);
+                    }
+                });
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkScanner.startPreview();
+//        if (MainActivity.cameraPermission) {
+//            checkScanner.startPreview();
+//        }
+    }
+
+    @Override
+    public void onPause() {
+        checkScanner.releaseResources();
+        super.onPause();
     }
 
 }
