e150c82c909f8d7b193baf4a03f0e9298cd64405
Daria Zelenova <muzzy404@gmail.com>
2018-12-24 19:02:15 +0300

    statistics cards clicks open corresponding activities

---
+++ app/src/main/java/spbpu/ponzelkoch/expensesadvisor/activities/LineChartActivity.java
@@ -0,0 +1,16 @@
+package spbpu.ponzelkoch.expensesadvisor.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import spbpu.ponzelkoch.expensesadvisor.R;
+
+public class LineChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_chart);
+    }
+}
--- app/src/main/java/spbpu/ponzelkoch/expensesadvisor/fragments/StatisticsFragment.java
+++ app/src/main/java/spbpu/ponzelkoch/expensesadvisor/fragments/StatisticsFragment.java
@@ -1,14 +1,20 @@
 package spbpu.ponzelkoch.expensesadvisor.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import spbpu.ponzelkoch.expensesadvisor.R;
+import spbpu.ponzelkoch.expensesadvisor.activities.PieChartActivity;
+
+import static spbpu.ponzelkoch.expensesadvisor.activities.LoginActivity.USERNAME;
 
 
 public class StatisticsFragment extends Fragment {
+
+    public static final String TITLE = "title";
 
     public StatisticsFragment() {
         // Required empty public constructor
@@ -18,7 +24,21 @@ public class StatisticsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        View root = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        // set listener to pie chart card to open pie chart activity by click
+        root.findViewById(R.id.statistics_pie_chart_card).setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), PieChartActivity.class);
+            startActivity(intent);
+        });
+
+        // set listener to line chart card to open pie line activity by click
+        root.findViewById(R.id.statistics_line_chart_card).setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), PieChartActivity.class);
+            startActivity(intent);
+        });
+
+        return root;
     }
 
 }
